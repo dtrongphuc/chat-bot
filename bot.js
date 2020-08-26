0f25fd6595f019a5f76f81c878686cb3897f8a23
dtrongphuc <dtrongphucdv@gmail.com>
2020-08-26 20:26:35 +0700

    change password

--- bot.js
+++ bot.js
@@ -1,67 +1,72 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const fs = require("fs");
 const login = require("facebook-chat-api");
-const mongoose = require('mongoose');
-var request = require('request');
-var Data = require('./models/data.model');
+const mongoose = require("mongoose");
+var request = require("request");
+var Data = require("./models/data.model");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 var answeredThreads = {};
 var botStatusThreads = {};
 var result = [];
 
 async function getData() {
-    var data = await Data.find();
-    result = await data[0].general;
-};
+	var data = await Data.find();
+	result = await data[0].general;
+}
 
-function findAndResponse (keyword) {
-    var resText;
-    if(!keyword) {
-        return;
-    }
-    var keywordLower = keyword.toLowerCase();
-    result.forEach(function(elem) {
-        let generalKey = Object.keys(elem)[0];
-        let keys = generalKey.split(", ");
-        for (const key of keys) {
-            if(keywordLower.includes(key)) {
-                var resArr = elem[generalKey];
-                let rng = Math.floor(Math.random() * resArr.length);
-                resText = `${resArr[rng]}`;
-            }       
-        };
-    });
-    return resText;
-};
+function findAndResponse(keyword) {
+	var resText;
+	if (!keyword) {
+		return;
+	}
+	var keywordLower = keyword.toLowerCase();
+	result.forEach(function (elem) {
+		let generalKey = Object.keys(elem)[0];
+		let keys = generalKey.split(", ");
+		for (const key of keys) {
+			if (keywordLower.includes(key)) {
+				var resArr = elem[generalKey];
+				let rng = Math.floor(Math.random() * resArr.length);
+				resText = `${resArr[rng]}`;
+			}
+		}
+	});
+	return resText;
+}
 
+login(
+	{
+		appState: JSON.parse(fs.readFileSync("appstate.json", "utf8")),
+	},
+	(err, api) => {
+		api.setOptions({
+			selfListen: false,
+			logLevel: "silent",
+			updatePresence: false,
+			userAgent:
+				"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36", //get cái này xem trong file login.js
+		});
 
-login({
-    appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))
- }, (err, api) => {
-
-    api.setOptions({
-        selfListen: false,
-        logLevel: "silent",
-        updatePresence: false,
-        userAgent: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36" //get cái này xem trong file login.js
-    });
-
-    if (err) return console.error(err);
-    api.listenMqtt(function callback(err, message){
-        getData();
-        var response = findAndResponse(message.body);
-        api.markAsRead(message.threadID);
-        if(response) {
-            answeredThreads[message.threadID] = true;
-            if(response === 'emoji sad') {
-                api.setMessageReaction('\uD83D\uDE22', message.messageID);
-                return;
-            }
-            api.sendMessage(`${response}`, message.threadID);
-        }
-        return;
-    });
-});
+		if (err) return console.error(err);
+		api.listenMqtt(function callback(err, message) {
+			getData();
+			var response = findAndResponse(message.body);
+			api.markAsRead(message.threadID);
+			if (response) {
+				answeredThreads[message.threadID] = true;
+				if (response === "emoji sad") {
+					api.setMessageReaction("\uD83D\uDE22", message.messageID);
+					return;
+				}
+				api.sendMessage(`${response}`, message.threadID);
+			}
+			return;
+		});
+	}
+);
--- login.js
+++ login.js
@@ -1,42 +1,38 @@
-﻿require('dotenv').config();
+﻿require("dotenv").config();
 
 const fs = require("fs");
 const login = require("facebook-chat-api");
 const readline = require("readline");
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout,
 });
 
 const option = {
-    logLevel:"silent",
-    forceLogin:true,
-    userAgent: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
-   //* cách lấy userAgent: F12-> tab console gõ 'navigator.userAgent' Link: https://imgur.com/oQ5hUkH
-}
+	logLevel: "silent",
+	forceLogin: true,
+	userAgent:
+		"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
+	//* cách lấy userAgent: F12-> tab console gõ 'navigator.userAgent' Link: https://imgur.com/oQ5hUkH
+};
 
-const obj = {email: process.env.EMAIL, password: process.env.PASSWORD};
+const obj = { email: process.env.EMAIL, password: process.env.PASSWORD };
 login(obj, option, (err, api) => {
-    if(err) {
-        switch (err.error) {
-            case 'login-approval':
-                console.log('Enter code > ');
-                rl.on('line', (line) => {
-                    err.continue(line);
-                    rl.close();
-                });
-                break;
-            default:
-                console.error(err);
-        }
-        return;
-    }
-    // Logged in wirite cookie!
-    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
-    
-     
+	if (err) {
+		switch (err.error) {
+			case "login-approval":
+				console.log("Enter code > ");
+				rl.on("line", (line) => {
+					err.continue(line);
+					rl.close();
+				});
+				break;
+			default:
+				console.error(err);
+		}
+		return;
+	}
+	// Logged in wirite cookie!
+	fs.writeFileSync("appstate.json", JSON.stringify(api.getAppState()));
 });
-
-
- 
